a9b46338b1ab2ff1378c229814d73f1d68f22418
Pedro Morgan <pedromorgan@gmail.com>
2016-06-09 20:44:45 +0100

    placeholders for a dream

--- ags4/unit.go
+++ ags4/unit.go
@@ -2,19 +2,63 @@
 
 package ags4
 
-// AGS Units are defined in
+// AGS Units are defined by AGS..
+// eg
+// "%" = "percentage"
+// "MPN/100ml" = "most probable number per 100 millilitres",
+// "DegC" = "degree Celsius"
+//
+// NOte that AGS is latin chars, so not of them windows
+// and unicodec haracters in there.. (which crashed my system)
+// So we need to validate that,
+// even though golang is UTF8.. SIGH!
+// But if we go to UTF-* then we can have a proper Degree^C symbol
+// uneditable by a windows users... sigh!!..
 
 
 
 type Unit struct {
+
+	// eg DegC, kN/m2  (latin remember)
 	Unit string 		` json:"unit" db:"unit" `
+
+	// eg kiloNewtons per square metre (latin)
 	Description string 	` json:"description" db:"description" `
+
+	// This is daffo special of OC. proper with UTF-8 symbol
+	Symbol string 	` json:"symbol" db:"symbol" `
 }
 
 // Returns true if the unit exists
+// Whist this is a "definition endevour"
+// we also want a typs and auto complete
 func UnitExists(unit string) bool {
 
-	// TO do is look up the
+	// TO do is look up the map for key
 
 	return false
+}
+
+// Unit autocomplete and valid an hints
+func UnitAutocomplete(txt string) (bool, []string){
+	// imagine the user has types in a k/m when K/m was means
+	// first we hekp with valid unit ?
+	yipee := UnitExists(txt)
+
+	hints := UnitsMatching(txt)
+
+	return yipee, hints
+
+}
+
+func UnitsMatching(txt string) []string {
+
+	matches := make(string,0,0)
+	// Do some clever searches
+	// of the memtree and terun some matchin, case insensitive
+
+
+	return matches
+
+
 }
