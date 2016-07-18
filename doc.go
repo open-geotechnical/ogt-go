e4606472b29243d875d822325ff7d887301bc668
Rubius <rubius@heurika.net>
2016-07-18 10:49:15 +0100

    added doc.go

---
+++ doc.go
@@ -0,0 +1,20 @@
+/*
+ID		Unique identifier across the project
+DT		Date time international format (see ISO 8601:2004)
+T		Elapsed time
+X		Text (non-numeric)
+XN		Text / Numeric (e.g. 1.00 or Dry)
+nDP		Value to n decimal places
+nSF		Value to n significant figures
+nSCI	Scientific notation to n decimal places
+PA		Text abbreviation taken from ABBR group
+PU		Text abbreviation taken from UNIT group
+PT		Text abbreviation taken from TYPE group
+U		Value with variable format
+MC		British Standard BS1377: Pt 2 reported moisture content
+DMS		Degrees: minutes: seconds
+YN		Y(es) or N(o)
+RL		Record Link
+*/
+
+package main
